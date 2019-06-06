b4cea0a1134a18c51e82ba888e5d100b300875cb
joan <joan@joan-PC>
2019-06-05 19:20:13 -0600

    pruebas unitarias pascal triangle terminadas

--- examenFinal-pascal-triangle/src/test/java/gt/edu/umg/ingenieria/sistemas/is/examenfinal/pascal/triangle/PascalTriangleTest.java
+++ examenFinal-pascal-triangle/src/test/java/gt/edu/umg/ingenieria/sistemas/is/examenfinal/pascal/triangle/PascalTriangleTest.java
@@ -59,9 +59,44 @@ public class PascalTriangleTest {
         
         int levels = 2;
         
-        String expResult = "[1[1, 1]]";
+        String expResult = "[1]\n[1, 1]\n";
         String result = instance.build(levels);
         assertEquals("Test #2 - Fallo",expResult, result);
     }
     
+    @Test
+    public void TestValorFloat() {
+        System.out.println("TEST #3 - Numero casteado de flotante");
+        PascalTriangle instance = new PascalTriangle();
+        
+        int levels = (int) 2.5;
+        
+        String expResult = "[1]\n[1, 1]\n";
+        String result = instance.build(levels);
+        assertEquals("Test #3 - Fallo",expResult, result);
+    }
+    
+    @Test
+    public void TestValorMaximo() {
+        System.out.println("TEST #4 - Valor Maximo");
+        PascalTriangle instance = new PascalTriangle();
+        
+        int levels = Integer.MAX_VALUE+1;
+        
+        String expResult = "[]";
+        String result = instance.build(levels);
+        assertEquals("Test #4 - Fallo",expResult, result);
+    }
+    
+    @Test
+    public void TestValorMinimo() {
+        System.out.println("TEST #5 - Valor Minimo");
+        PascalTriangle instance = new PascalTriangle();
+        
+        int levels = Integer.MIN_VALUE;
+        
+        String expResult = "[]";
+        String result = instance.build(levels);
+        assertEquals("Test #5 - Fallo",expResult, result);
+    }
 }
